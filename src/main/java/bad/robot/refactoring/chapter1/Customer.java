f6d63e4b7bd85cfdb864c9a557fd6c42dddb1d11
Kadynce DeFrancesco <defrank1@ferris.edu>
2024-04-02 22:26:41 -0400

    Extract Method

--- src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -27,11 +27,7 @@ public class Customer {
 
         String result = "Rental record for " + getName() + "\n";
         for (Rental rental : rentals) {
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints = calculateFrequentRenterPoints(frequentRenterPoints, rental);
 
             // show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getChargeFor()) + "\n";
@@ -44,4 +40,13 @@ public class Customer {
 
         return result;
     }
+
+	private int calculateFrequentRenterPoints(int frequentRenterPoints, Rental rental) {
+		// add frequent renter points
+		frequentRenterPoints++;
+		// add bonus for a two day new release rental
+		if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
+		    frequentRenterPoints++;
+		return frequentRenterPoints;
+	}
 }
